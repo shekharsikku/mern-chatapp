704a8d7ab3c1e88e0c380f24fa6c218b5c6e8ded
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2024-03-10 09:32:24 +0530

    Signup User Functionality Completed

--- client/src/pages/signup/Signup.jsx
+++ client/src/pages/signup/Signup.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import useSignup from "../../hooks/useSignup";
 import Gender from "./Gender";
 
 const Signup = () => {
+  const [inputs, setInputs] = useState({
+    fullname: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+  });
+
+  const handleCheckboxChange = (gender) => {
+    setInputs({ ...inputs, gender });
+  };
+
+  const { loading, signup } = useSignup();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await signup(inputs);
+  };
+
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
       <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
@@ -8,19 +30,35 @@ const Signup = () => {
           Sign Up <span className='text-blue-500'> ChatApp</span>
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className='label p-2'>
               <span className='text-base label-text'>Full Name</span>
             </label>
-            <input type='text' placeholder='John Doe' className='w-full input input-bordered  h-10' />
+            <input
+              type='text'
+              placeholder='Fullname'
+              className='w-full input input-bordered  h-10'
+              value={inputs.fullname}
+              onChange={(e) => setInputs({ ...inputs, fullname: e.target.value })}
+              name="fullname"
+              autoComplete="off"
+            />
           </div>
 
           <div>
             <label className='label p-2 '>
               <span className='text-base label-text'>Username</span>
             </label>
-            <input type='text' placeholder='johndoe' className='w-full input input-bordered h-10' />
+            <input
+              type='text'
+              placeholder='Username'
+              className='w-full input input-bordered h-10'
+              value={inputs.username}
+              onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
+              name="username"
+              autoComplete="off"
+            />
           </div>
 
           <div>
@@ -31,6 +69,10 @@ const Signup = () => {
               type='password'
               placeholder='Enter Password'
               className='w-full input input-bordered h-10'
+              value={inputs.password}
+              onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+              name="password"
+              autoComplete="off"
             />
           </div>
 
@@ -42,17 +84,23 @@ const Signup = () => {
               type='password'
               placeholder='Confirm Password'
               className='w-full input input-bordered h-10'
+              value={inputs.confirmPassword}
+              onChange={(e) => setInputs({ ...inputs, confirmPassword: e.target.value })}
+              name="confirmPassword"
+              autoComplete="off"
             />
           </div>
 
-          <Gender />
+          <Gender onCheckboxChange={handleCheckboxChange} selectedGender={inputs.gender} />
 
-          <a className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block' href='#'>
+          <Link to={"/signin"} className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
             Already have an account?
-          </a>
+          </Link>
 
           <div>
-            <button className='btn btn-block btn-sm mt-2 border border-slate-700'>Sign Up</button>
+            <button className='btn btn-block btn-sm mt-2 border border-slate-700' disabled={loading}>
+              {loading ? <span className='loading loading-spinner'></span> : "Sign Up"}
+            </button>
           </div>
         </form>
       </div>
