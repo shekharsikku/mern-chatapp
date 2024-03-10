0a118f53a2ed6f2afc72e49e383c56a967d4de1f
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2024-03-10 08:54:59 +0530

    UI Design Almost Completed

--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -1,8 +1,22 @@
+import Message from "./Message";
 
 const Messages = () => {
   return (
-    <div>Messages</div>
+    <div className='px-4 flex-1 overflow-auto'>
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
   )
 }
 
-export default Messages
+export default Messages;
