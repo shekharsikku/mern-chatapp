3eaaa879d9f0464efd37652d08e27246d97e6223
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2024-03-10 12:38:30 +0530

    Realtime Message Functionality Activated from Backend

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -1,6 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
-// import { getReceiverSocketId, io } from "../socket/socket.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 const sendMessage = async (req, res) => {
   try {
@@ -34,12 +34,12 @@ const sendMessage = async (req, res) => {
     // this will run in parallel
     await Promise.all([conversation.save(), newMessage.save()]);
 
-    // socketio functionality
-    // const receiverSocketId = getReceiverSocketId(receiverId);
-    // if (receiverSocketId) {
-    //   // io.to(<socket_id>).emit() used to send events to specific client
-    //   io.to(receiverSocketId).emit("newMessage", newMessage);
-    // }
+    // socketio functionality for realtime messaging
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      // io.to(<socket_id>).emit() used to send events to specific client
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     res.status(201).json(newMessage);
   } catch (error) {
